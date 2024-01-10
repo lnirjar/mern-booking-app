2d7e592cdd9cf14df6a0017d814b59dc77a47334
lnirjar <lnirjar@gmail.com>
2024-01-10 18:13:30 +0530

    completed header

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route index element={<IndexPage />} />
+    </Routes>
+  )
+}
+
+export default App
